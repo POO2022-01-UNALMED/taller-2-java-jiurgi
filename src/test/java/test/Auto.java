380880c0ed4a311f6132ddc8dd3aeb7ade8cb46d
Jimena Uribe Giraldo <jiuribeg@unal.edu.co>
2022-03-21 22:43:01 -0500

    Codigo

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,35 @@
+package test;
+public class Auto {
+	String modelo;
+	int precio;
+	Asiento[] asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	static int cantidadCreados;
+	int cantidadAsientos() {
+		int asien=0;
+		for(int i=0; i < asientos.length; i++) {
+			if(asientos[i]!= null) 
+			{
+				asien++;
+			}
+		}
+		return asien;
+		}
+	String verificarIntegridad() {
+		if(registro==motor.registro) {
+			for(Asiento asiento:asientos) {
+				if(asiento!= null) {
+					if(registro!= asiento.registro) {
+						return "Las piezas no son originales";
+					}
+				}
+			}
+			return "Auto original";
+		}
+		return "Las piezas no son originales";
+	}		
+	}
+
+
